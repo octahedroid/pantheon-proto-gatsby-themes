4874e13cfe445fd25e174d70240e76f385fa802d
jmolivas <jesus.olivas@gmail.com>
2020-01-02 09:01:22 -0800

    feat: add gatsby-drupal-starter

---
+++ gatsby-drupal/gatsby-config.js
@@ -0,0 +1,26 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby site with Drupal`,
+    description: `Gatsby site with Drupal`,
+    author: `@octahedroid`,
+    github: `https://github.com/octahedroid`,
+    blog: `/#`,
+    product: `/#`,
+    docs: `/#`,
+    siteUrl: 'http://example.com'
+  },
+  plugins: [
+    {
+      resolve: `gatsby-theme-pantheon-drupal`,
+      options: {
+        root: __dirname,
+      }
+    }
+  ]
+}
