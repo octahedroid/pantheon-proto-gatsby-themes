71f58edb89c9f8194971c355e19f16e312af4f64
Omar Aguirre <omersguchigu@gmail.com>
2020-03-24 19:10:27 -0600

    perf(upgrade): major upgrade of node dependencies

--- packages/gatsby-theme-pantheon-core/theme.js
+++ packages/gatsby-theme-pantheon-core/theme.js
@@ -1,138 +1,137 @@
-
 const borderWidths = {
-  px: '1px',
-  '0': '0',
-  '2': '2px',
-  '4': '4px',
-  '8': '8px',
-}
+  px: "1px",
+  "0": "0",
+  "2": "2px",
+  "4": "4px",
+  "8": "8px"
+};
 
-const breakpoints = ['640px', '768px', '1024px', '1280px', '1440px']
+const breakpoints = ["640px", "768px", "1024px", "1280px", "1440px"];
 
 const baseColors = {
-  transparent: 'transparent',
-  black: '#000',
-  white: '#fff',
+  transparent: "transparent",
+  black: "#000",
+  white: "#fff",
 
   gray: {
-    100: '#f7fafc',
-    200: '#edf2f7',
-    300: '#e2e8f0',
-    400: '#cbd5e0',
-    500: '#a0aec0',
-    600: '#718096',
-    700: '#4a5568',
-    800: '#2d3748',
-    900: '#1a202c',
+    100: "#f7fafc",
+    200: "#edf2f7",
+    300: "#e2e8f0",
+    400: "#cbd5e0",
+    500: "#a0aec0",
+    600: "#718096",
+    700: "#4a5568",
+    800: "#2d3748",
+    900: "#1a202c"
   },
   red: {
-    100: '#fff5f5',
-    200: '#fed7d7',
-    300: '#feb2b2',
-    400: '#fc8181',
-    500: '#f56565',
-    600: '#e53e3e',
-    700: '#c53030',
-    800: '#9b2c2c',
-    900: '#742a2a',
+    100: "#fff5f5",
+    200: "#fed7d7",
+    300: "#feb2b2",
+    400: "#fc8181",
+    500: "#f56565",
+    600: "#e53e3e",
+    700: "#c53030",
+    800: "#9b2c2c",
+    900: "#742a2a"
   },
   orange: {
-    100: '#fffaf0',
-    200: '#feebc8',
-    300: '#fbd38d',
-    400: '#f6ad55',
-    500: '#ed8936',
-    600: '#dd6b20',
-    700: '#c05621',
-    800: '#9c4221',
-    900: '#7b341e',
+    100: "#fffaf0",
+    200: "#feebc8",
+    300: "#fbd38d",
+    400: "#f6ad55",
+    500: "#ed8936",
+    600: "#dd6b20",
+    700: "#c05621",
+    800: "#9c4221",
+    900: "#7b341e"
   },
   yellow: {
-    100: '#fffff0',
-    200: '#fefcbf',
-    300: '#faf089',
-    400: '#f6e05e',
-    500: '#ecc94b',
-    600: '#d69e2e',
-    700: '#b7791f',
-    800: '#975a16',
-    900: '#744210',
+    100: "#fffff0",
+    200: "#fefcbf",
+    300: "#faf089",
+    400: "#f6e05e",
+    500: "#ecc94b",
+    600: "#d69e2e",
+    700: "#b7791f",
+    800: "#975a16",
+    900: "#744210"
   },
   green: {
-    100: '#f0fff4',
-    200: '#c6f6d5',
-    300: '#9ae6b4',
-    400: '#68d391',
-    500: '#48bb78',
-    600: '#38a169',
-    700: '#2f855a',
-    800: '#276749',
-    900: '#22543d',
+    100: "#f0fff4",
+    200: "#c6f6d5",
+    300: "#9ae6b4",
+    400: "#68d391",
+    500: "#48bb78",
+    600: "#38a169",
+    700: "#2f855a",
+    800: "#276749",
+    900: "#22543d"
   },
   teal: {
-    100: '#e6fffa',
-    200: '#b2f5ea',
-    300: '#81e6d9',
-    400: '#4fd1c5',
-    500: '#38b2ac',
-    600: '#319795',
-    700: '#2c7a7b',
-    800: '#285e61',
-    900: '#234e52',
+    100: "#e6fffa",
+    200: "#b2f5ea",
+    300: "#81e6d9",
+    400: "#4fd1c5",
+    500: "#38b2ac",
+    600: "#319795",
+    700: "#2c7a7b",
+    800: "#285e61",
+    900: "#234e52"
   },
   blue: {
-    100: '#ebf8ff',
-    200: '#bee3f8',
-    300: '#90cdf4',
-    400: '#63b3ed',
-    500: '#4299e1',
-    600: '#3182ce',
-    700: '#2b6cb0',
-    800: '#2c5282',
-    900: '#2a4365',
+    100: "#ebf8ff",
+    200: "#bee3f8",
+    300: "#90cdf4",
+    400: "#63b3ed",
+    500: "#4299e1",
+    600: "#3182ce",
+    700: "#2b6cb0",
+    800: "#2c5282",
+    900: "#2a4365"
   },
   indigo: {
-    100: '#ebf4ff',
-    200: '#c3dafe',
-    300: '#a3bffa',
-    400: '#7f9cf5',
-    500: '#667eea',
-    600: '#5a67d8',
-    700: '#4c51bf',
-    800: '#434190',
-    900: '#3c366b',
+    100: "#ebf4ff",
+    200: "#c3dafe",
+    300: "#a3bffa",
+    400: "#7f9cf5",
+    500: "#667eea",
+    600: "#5a67d8",
+    700: "#4c51bf",
+    800: "#434190",
+    900: "#3c366b"
   },
   purple: {
-    100: '#faf5ff',
-    200: '#e9d8fd',
-    300: '#d6bcfa',
-    400: '#b794f4',
-    500: '#9f7aea',
-    600: '#805ad5',
-    700: '#6b46c1',
-    800: '#553c9a',
-    900: '#44337a',
+    100: "#faf5ff",
+    200: "#e9d8fd",
+    300: "#d6bcfa",
+    400: "#b794f4",
+    500: "#9f7aea",
+    600: "#805ad5",
+    700: "#6b46c1",
+    800: "#553c9a",
+    900: "#44337a"
   },
   pink: {
-    100: '#fff5f7',
-    200: '#fed7e2',
-    300: '#fbb6ce',
-    400: '#f687b3',
-    500: '#ed64a6',
-    600: '#d53f8c',
-    700: '#b83280',
-    800: '#97266d',
-    900: '#702459',
-  },
-}
+    100: "#fff5f7",
+    200: "#fed7e2",
+    300: "#fbb6ce",
+    400: "#f687b3",
+    500: "#ed64a6",
+    600: "#d53f8c",
+    700: "#b83280",
+    800: "#97266d",
+    900: "#702459"
+  }
+};
 
 const commonButtonStyles = {
   py: 2,
   px: 3,
   cursor: `pointer`,
   fontSize: `100%`,
-  lineHeight: `inherit`,
-}
+  lineHeight: `inherit`
+};
 
 const buttons = {
   simple: {
@@ -142,9 +141,9 @@ const buttons = {
     color: `white`,
     fontWeight: `bold`,
     borderRadius: `default`,
-    '&:hover': {
-      backgroundColor: `primaryHover`,
-    },
+    "&:hover": {
+      backgroundColor: `primaryHover`
+    }
   },
   pill: {
     ...commonButtonStyles,
@@ -153,9 +152,9 @@ const buttons = {
     color: `white`,
     fontWeight: `bold`,
     borderRadius: `full`,
-    '&:hover': {
-      backgroundColor: `primaryHover`,
-    },
+    "&:hover": {
+      backgroundColor: `primaryHover`
+    }
   },
   outline: {
     ...commonButtonStyles,
@@ -166,11 +165,11 @@ const buttons = {
     color: `primary`,
     fontWeight: `semibold`,
     borderRadius: `default`,
-    '&:hover': {
+    "&:hover": {
       backgroundColor: `primary`,
       color: `white`,
-      borderColor: `transparent`,
-    },
+      borderColor: `transparent`
+    }
   },
   bordered: {
     ...commonButtonStyles,
@@ -181,9 +180,9 @@ const buttons = {
     color: `white`,
     fontWeight: `bold`,
     borderRadius: `default`,
-    '&:hover': {
-      backgroundColor: `primaryHover`,
-    },
+    "&:hover": {
+      backgroundColor: `primaryHover`
+    }
   },
   disabled: {
     ...commonButtonStyles,
@@ -193,9 +192,9 @@ const buttons = {
     cursor: `not-allowed`,
     color: `white`,
     fontWeight: `bold`,
-    borderRadius: `default`,
+    borderRadius: `default`
   },
-  '3D': {
+  "3D": {
     ...commonButtonStyles,
     backgroundColor: `primary`,
     border: `none`,
@@ -206,9 +205,9 @@ const buttons = {
     fontWeight: `bold`,
     borderRadius: `default`,
     transition: `transform 0.3s ease-in-out`,
-    '&:hover': {
-      transform: `translateY(-1px)`,
-    },
+    "&:hover": {
+      transform: `translateY(-1px)`
+    }
   },
   elevated: {
     ...commonButtonStyles,
@@ -220,21 +219,22 @@ const buttons = {
     fontWeight: `bold`,
     borderRadius: `default`,
     boxShadow: `default`,
-    '&:hover': {
-      backgroundColor: `gray.1`,
-    },
-  },
-}
+    "&:hover": {
+      backgroundColor: `gray.1`
+    }
+  }
+};
 
 const colors = {
   ...baseColors,
-  primary: '#0CBFC7',
-  secondary: '#CA3E73',
-  accent: '#E8DA8B',
-  darkShade: '#37375B',
-  lightShade: '#F7F7F7',
-  body: 'darkShade'
-}
+  primary: "#0CBFC7",
+  background: "white",
+  secondary: "#CA3E73",
+  accent: "#E8DA8B",
+  darkShade: "#37375B",
+  lightShade: "#F7F7F7",
+  body: "darkShade"
+};
 
 const baseFonts = {
   sans:
@@ -243,57 +243,57 @@ const baseFonts = {
   mono: 'Menlo,Monaco,Consolas,"Liberation Mono","Courier New",monospace',
   alternative: "'Montserrat', sans-serif",
   blog: "'Josefin Sans', sans-serif"
-}
+};
 
 const fonts = {
   ...baseFonts,
   body: baseFonts.sans,
   heading: baseFonts.sans,
   monospace: baseFonts.mono,
-  alternative: baseFonts.alternative,
-}
+  alternative: baseFonts.alternative
+};
 
 const fontSizes = {
-  'xs': '.75rem',
-  'sm': '.875rem',
-  'tiny': '.875rem',
-  'base': '1rem',
-  'lg': '1.125rem',
-  'xl': '1.25rem',
-  '2xl': '1.5rem',
-  '3xl': '1.875rem',
-  '4xl': '2.25rem',
-  '5xl': '3rem',
-  '6xl': '4rem',
-  '7xl': '5rem',
-  '8xl': '6rem',
-  '9xl': '7rem',
-  '10xl': '8rem',
-  'h1': '1.875rem',
-  'h2': '1.5rem',
-  'h3': '1.25rem',
-  'h4': '1.125rem',
-  'h5': '1rem',
-  'h6': '0.875rem',
-}
+  xs: ".75rem",
+  sm: ".875rem",
+  tiny: ".875rem",
+  base: "1rem",
+  lg: "1.125rem",
+  xl: "1.25rem",
+  "2xl": "1.5rem",
+  "3xl": "1.875rem",
+  "4xl": "2.25rem",
+  "5xl": "3rem",
+  "6xl": "4rem",
+  "7xl": "5rem",
+  "8xl": "6rem",
+  "9xl": "7rem",
+  "10xl": "8rem",
+  h1: "1.875rem",
+  h2: "1.5rem",
+  h3: "1.25rem",
+  h4: "1.125rem",
+  h5: "1rem",
+  h6: "0.875rem"
+};
 
 const baseFontWeights = {
-  hairline: '100',
-  thin: '200',
-  light: '300',
-  normal: '400',
-  medium: '500',
-  semibold: '600',
-  bold: '700',
-  extrabold: '800',
-  black: '900',
-}
+  hairline: "100",
+  thin: "200",
+  light: "300",
+  normal: "400",
+  medium: "500",
+  semibold: "600",
+  bold: "700",
+  extrabold: "800",
+  black: "900"
+};
 
 const fontWeights = {
   ...baseFontWeights,
   body: baseFontWeights.normal,
-  heading: baseFontWeights.bold,
-}
+  heading: baseFontWeights.bold
+};
 
 const commonInputStyles = {
   py: 2,
@@ -301,8 +301,8 @@ const commonInputStyles = {
   fontSize: `100%`,
   borderRadius: `default`,
   appearance: `none`,
-  lineHeight: `tight`,
-}
+  lineHeight: `tight`
+};
 
 const inputs = {
   shadow: {
@@ -310,10 +310,10 @@ const inputs = {
     border: `none`,
     color: `gray.7`,
     boxShadow: `default`,
-    '&:focus': {
+    "&:focus": {
       outline: `none`,
-      boxShadow: `outline`,
-    },
+      boxShadow: `outline`
+    }
   },
   inline: {
     ...commonInputStyles,
@@ -322,11 +322,11 @@ const inputs = {
     borderStyle: `solid`,
     borderColor: `gray.2`,
     color: `gray.7`,
-    '&:focus': {
+    "&:focus": {
       outline: `none`,
       borderColor: `primary`,
-      backgroundColor: `white`,
-    },
+      backgroundColor: `white`
+    }
   },
   underline: {
     ...commonInputStyles,
@@ -337,179 +337,179 @@ const inputs = {
     borderBottomColor: `primary`,
     borderRadius: `0px`,
     color: `gray.7`,
-    '&:focus': {
+    "&:focus": {
       outline: `none`,
       borderColor: `primary`,
-      backgroundColor: `white`,
-    },
-  },
-}
+      backgroundColor: `white`
+    }
+  }
+};
 
 const letterSpacings = {
-  tighter: '-0.05em',
-  tight: '-0.025em',
-  normal: '0',
-  wide: '0.025em',
-  wider: '0.05em',
-  widest: '0.1em',
-}
+  tighter: "-0.05em",
+  tight: "-0.025em",
+  normal: "0",
+  wide: "0.025em",
+  wider: "0.05em",
+  widest: "0.1em"
+};
 
 const baseLineHeights = {
-  none: '1',
-  tight: '1.25',
-  snug: '1.375',
-  normal: '1.5',
-  relaxed: '1.625',
-  loose: '2',
-}
+  none: "1",
+  tight: "1.25",
+  snug: "1.375",
+  normal: "1.5",
+  relaxed: "1.625",
+  loose: "2"
+};
 
 const lineHeights = {
   ...baseLineHeights,
   body: baseLineHeights.relaxed,
-  heading: baseLineHeights.tight,
-}
+  heading: baseLineHeights.tight
+};
 
 const radii = {
-  none: '0',
-  sm: '0.125rem',
-  default: '0.25rem',
-  lg: '0.5rem',
-  full: '9999px',
-}
+  none: "0",
+  sm: "0.125rem",
+  default: "0.25rem",
+  lg: "0.5rem",
+  full: "9999px"
+};
 
 const spacing = {
-  px: '1px',
-  '0': '0',
-  '1': '0.25rem',
-  '2': '0.5rem',
-  '3': '0.75rem',
-  '4': '1rem',
-  '5': '1.25rem',
-  '6': '1.5rem',
-  '8': '2rem',
-  '10': '2.5rem',
-  '12': '3rem',
-  '16': '4rem',
-  '20': '5rem',
-  '24': '6rem',
-  '32': '8rem',
-  '40': '10rem',
-  '48': '12rem',
-  '56': '14rem',
-  '64': '16rem',
-  '72': '26rem',
-  '80': '36rem',
-  navbar: '100px',
-}
+  px: "1px",
+  "0": "0",
+  "1": "0.25rem",
+  "2": "0.5rem",
+  "3": "0.75rem",
+  "4": "1rem",
+  "5": "1.25rem",
+  "6": "1.5rem",
+  "8": "2rem",
+  "10": "2.5rem",
+  "12": "3rem",
+  "16": "4rem",
+  "20": "5rem",
+  "24": "6rem",
+  "32": "8rem",
+  "40": "10rem",
+  "48": "12rem",
+  "56": "14rem",
+  "64": "16rem",
+  "72": "26rem",
+  "80": "36rem",
+  navbar: "100px"
+};
 
 const height = {
   ...spacing,
-  screen: '100vh',
-  navbar: '80px',
-  hero: '800px'
+  screen: "100vh",
+  navbar: "80px",
+  hero: "800px"
 };
 const shadows = {
-  default: '0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)',
-  md: '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
-  lg: '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
+  default: "0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)",
+  md: "0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)",
+  lg: "0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)",
   xl:
-    '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)',
-  '2xl': '0 25px 50px -12px rgba(0, 0, 0, 0.25)',
-  inner: 'inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)',
-  outline: '0 0 0 3px rgba(66, 153, 225, 0.5)',
-  none: 'none',
-}
+    "0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)",
+  "2xl": "0 25px 50px -12px rgba(0, 0, 0, 0.25)",
+  inner: "inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)",
+  outline: "0 0 0 3px rgba(66, 153, 225, 0.5)",
+  none: "none"
+};
 
 const space = [
   0,
-  '0.25rem',
-  '0.5rem',
-  '1rem',
-  '2rem',
-  '4rem',
-  '8rem',
-  '16rem',
-  '32rem',
-]
+  "0.25rem",
+  "0.5rem",
+  "1rem",
+  "2rem",
+  "4rem",
+  "8rem",
+  "16rem",
+  "32rem"
+];
 
 const zIndices = {
-  auto: 'auto',
-  '0': '0',
-  '10': '10',
-  '20': '20',
-  '30': '30',
-  '40': '40',
-  '50': '50',
-}
+  auto: "auto",
+  "0": "0",
+  "10": "10",
+  "20": "20",
+  "30": "30",
+  "40": "40",
+  "50": "50"
+};
 
 const heading = {
-  fontFamily: 'heading',
-  fontWeight: 'heading',
-  lineHeight: 'heading',
-}
+  fontFamily: "heading",
+  fontWeight: "heading",
+  lineHeight: "heading"
+};
 
 const transitions = {
-  color: 'color, border-color .4s ease-in-out',
-  bg: 'color, background-color .2s ease-in-out',
-  shadow: 'box-shadow .4s ease-in-out',
-  filter: 'filter .4s ease-in-out',
-}
+  color: "color, border-color .4s ease-in-out",
+  bg: "color, background-color .2s ease-in-out",
+  shadow: "box-shadow .4s ease-in-out",
+  filter: "filter .4s ease-in-out"
+};
 
 const styles = {
   root: {
-    fontFamily: 'body',
-    lineHeight: 'body',
-    fontWeight: 'body',
+    fontFamily: "body",
+    lineHeight: "body",
+    fontWeight: "body"
   },
   a: {
-    color: 'primary',
-    textDecoration: 'none',
-    ':hover': {
-      textDecoration: 'underline',
-    },
+    color: "primary",
+    textDecoration: "none",
+    ":hover": {
+      textDecoration: "underline"
+    }
   },
   p: {
-    fontSize: 'xl',
-    color: 'body',
-    mb: 2,
+    fontSize: "xl",
+    color: "body",
+    mb: 2
   },
   h1: {
     ...heading,
-    fontSize: "3rem",
+    fontSize: "3rem"
   },
   h2: {
     ...heading,
-    fontSize: "2.5rem",
+    fontSize: "2.5rem"
   },
   h3: {
     ...heading,
-    fontSize: "2rem",
+    fontSize: "2rem"
   },
   h4: {
     ...heading,
-    fontSize: "1.5rem",
+    fontSize: "1.5rem"
   },
   h5: {
     ...heading,
     fontSize: "1.25rem",
-    fontWeight: 'body',
+    fontWeight: "body"
   },
   h6: {
     ...heading,
-    fontSize: "1rem",
+    fontSize: "1rem"
   },
   code: {},
   pre: {},
   hr: {
-    bg: 'muted',
+    bg: "muted",
     border: 0,
-    height: '1px',
-    m: 3,
-  },
-}
+    height: "1px",
+    m: 3
+  }
+};
 const variants = {
-  borderWidth: ['responsive', 'first', 'hover', 'focus'],
-  backgroundColor: ['responsive', 'hover', 'focus'],
+  borderWidth: ["responsive", "first", "hover", "focus"],
+  backgroundColor: ["responsive", "hover", "focus"]
 };
 
 const theme = {
@@ -532,6 +532,6 @@ const theme = {
   variants,
   height,
   transitions
-}
+};
 
 module.exports = theme;
